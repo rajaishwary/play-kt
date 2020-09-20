c94b81b17e908d9c040dd32a5c353d06e5d1509c
rajaishwary <rajaishwary@gmail.com>
2020-09-21 04:29:50 +0530

    support libs and image view

--- app/src/main/java/com/codalyze/playkt/MainActivity.kt
+++ app/src/main/java/com/codalyze/playkt/MainActivity.kt
@@ -2,16 +2,33 @@ package com.codalyze.playkt
 
 import android.os.Bundle
 import android.widget.Button
-import android.widget.Toast
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+    lateinit var diceImg: ImageView;
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val rollButton: Button = findViewById(R.id.roll_button);
         rollButton.setOnClickListener {
-            Toast.makeText(this, "Button Clicked.", Toast.LENGTH_LONG).show();
+            rollDice();
         }
+        diceImg = findViewById(R.id.dice_image);
+    }
+
+    private fun rollDice() {
+        val diceRes = when (Random.nextInt(1, 7)) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            6 -> R.drawable.dice_6
+            else -> R.drawable.empty_dice
+        }
+        diceImg.setImageResource(diceRes);
     }
 }
